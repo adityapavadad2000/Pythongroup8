be676cccc2784d486e577cecfce7516ea7f8ddde
Jelmer van Gestel <J.H.F.vangestel@student.tudelft.nl>
2023-11-10 11:46:56 +0100

    Fixed graph, added overall conclusions and added code/data

---
+++ data/TOTAL - Goodstransport Plot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import plotly.express as px
+
+# Read data from the Excel file
+file_path = "TOTAL - Goodstransport_mld_ton.xlsx"
+df = pd.read_excel(file_path)
+
+# Create a line chart
+fig = px.line(df, x='Year', y=df.columns[1:4], title='Total Transported Goods Over the Years',
+              labels={'value': 'Total Transported Goods (kg)', 'variable': 'Transportation Mode'},
+              color_discrete_sequence=px.colors.qualitative.Set1)
+
+# Show the plot
+fig.show()
